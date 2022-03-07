845ac1a5263215e1f071f682a23cd74e1ec73901
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-03-07 12:08:00 +0800

    Insert index and acript filese

---
+++ assets/js/Questions.js
@@ -0,0 +1,168 @@
+var questionList=[
+    {
+        question:"Inside which HTML element do we put the JavaScript?",
+        options:[{
+            choice:"<js>",
+            isAnswer:false
+        },
+        {
+            choice:"<script>",
+            isAnswer:true
+        },
+        {
+            choice:"<scripting>",
+            isAnswer:false
+        },
+        {
+            choice:"<javascript>",
+            isAnswer:false
+        }]
+    },
+    {
+        question:"Where is the correct place to insert a JavaScript?",
+        options:[{
+            choice:"Both the <head> section and <body> section are correct",
+            isAnswer:true
+        },
+        {
+            choice:"The <body> section",
+            isAnswer:false
+        },
+        {
+            choice:"The <head> section",
+            isAnswer:false
+        }]
+    },
+    {
+        question:"What is the correct syntax for referring to an external script called 'xxx.js'?",
+        options:[{
+            choice:"<script src='xxx.js'>",
+            isAnswer:true
+        },
+        {
+            choice:"<script name='xxx.js'>",
+            isAnswer:false
+        },
+        {
+            choice:"<script href='xxx.js'>",
+            isAnswer:false
+        }]
+    },
+    {
+        question:"The external JavaScript file must contain the <script> tag?",
+        options:[{
+            choice:"True",
+            isAnswer:true
+        },
+        {
+            choice:"False",
+            isAnswer:false
+        }]
+    },
+    {
+        question:"How do you write 'Hello World' in an alert box?",
+        options:[{
+            choice:"msg(“Hello World”)",
+            isAnswer:false
+        },
+        {
+            choice:"alertBox(“Hello World”)",
+            isAnswer:false
+        },
+        {
+            choice:"msgBox(“Hello World”)",
+            isAnswer:false
+        },
+        {
+            choice:"alert(“Hello World”)",
+            isAnswer:true
+        }]
+    },
+    {
+        question:"How do you create a function in JavaScript?",
+        options:[{
+            choice:"function myfunction()",
+            isAnswer:true
+        },
+        {
+            choice:"function=myfunction()",
+            isAnswer:false
+        },
+        {
+            choice:"function:myfunction()",
+            isAnswer:false
+        }]
+    },
+    {
+        question:"How do you call a function named 'myFunction'?",
+        options:[{
+            choice:"call myFunction()",
+            isAnswer:false
+        },
+        {
+            choice:"myFunction()",
+            isAnswer:true
+        },
+        {
+            choice:"call function myFunction()",
+            isAnswer:false
+        }]
+    },
+    {
+        question:"How to write an IF statement in JavaScript?",
+        options:[{
+            choice:"if i=5 then",
+            isAnswer:false
+        },
+        {
+            choice:"if(i==5)",
+            isAnswer:true
+        },
+        {
+            choice:"if i==5 then",
+            isAnswer:false
+        },
+        {
+            choice:"if i=5 ",
+            isAnswer:false
+        }]
+    },
+    {
+        question:"How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        options:[{
+            choice:"if i<>5",
+            isAnswer:false
+        },
+        {
+            choice:"if i!=5 then",
+            isAnswer:false
+        },
+        {
+            choice:"if(i!=5)",
+            isAnswer:true
+        },
+        {
+            choice:"if(i<>5)",
+            isAnswer:false
+        }]
+    },
+    {
+        question:"How does a FOR loop start?",
+        options:[{
+            choice:"for(i=0;i<5)",
+            isAnswer:false
+        },
+        {
+            choice:"for(i<=5;i++)",
+            isAnswer:false
+        },
+        {
+            choice:"for(i=0;i<=5;i++)",
+            isAnswer:true
+        },
+        {
+            choice:"for i=0 to 5",
+            isAnswer:false
+        }]
+    }
+];
---
+++ assets/js/script.js
@@ -0,0 +1,255 @@
+var startButton = document.querySelector("#btnStartGame");
+var questionDiv = document.querySelector("#question-div");
+var timer_div = document.querySelector("#timer-div");
+var record_name = document.querySelector("#record-name");
+var highscore = document.querySelector("#highscore");
+var playername = document.querySelector("#name");
+var btnClearHistory = document.querySelector("#btnClearHistory");
+
+var btnSave = document.querySelector("#btnSave");
+var high_score_div = document.querySelector("#high-score-div");
+var timer;
+var timerCount;
+var isWin = false;
+var totalQuestion = 0;
+var totalCorrect = 0;
+var currentquestionid = 0;
+var seconremaining = 0;
+const wrongAnserdeduction = 2;
+
+// helper function to create table
+function CreateTable() {
+  var table = document.createElement("table");
+  table.setAttribute("id", "Player");
+  var row = document.createElement("tr");
+  var column1 = document.createElement("th");
+  var column2 = document.createElement("th");
+  var column3 = document.createElement("th");
+  var column4 = document.createElement("th");
+  column1.textContent = "Name";
+  column2.textContent = "Question Attempt";
+  column3.textContent = "Answer Correct";
+  column4.textContent = "Seconds Remaining";
+  row.appendChild(column1);
+  row.appendChild(column2);
+  row.appendChild(column3);
+  row.appendChild(column4);
+  table.appendChild(row);
+  return table;
+}
+
+function isEmpty(value) {
+  return value == undefined || value == null || value.length === 0;
+}
+
+//will execute when page loads initially
+function init() {
+  btnClearHistory.classList.add("hide");
+  questionDiv.classList.add("hide");
+  timer_div.classList.add("hide");
+  record_name.classList.add("hide");
+  high_score_div.classList.add("hide");
+  return;
+}
+
+//will execute when start game button click
+function startgame(event) {
+  timerCount = 10;
+  totalQuestion = 0;
+  totalCorrect = 0;
+  seconremaining = 0;
+  currentquestionid = 0;
+  isWin = false;
+  startButton.classList.add("hide");
+  questionDiv.classList.remove("hide");
+
+  var returnpage = startButton.getAttribute("data-return");
+  //check button click come from play again or start game
+  if (returnpage == "false") {
+  } else {
+    high_score_div.classList.add("hide");
+    btnClearHistory.classList.add("hide");
+    playername.value = "";
+    timer_div.textContent = "";
+  }
+  LoadQuestion(currentquestionid);
+  startTimer();
+  timer_div.classList.remove("hide");
+  return;
+}
+
+//start timer
+function startTimer() {
+  timer = setInterval(function () {
+    timerCount--;
+    timer_div.textContent = timerCount;
+    if (timerCount > 0) {
+      if (isWin && timerCount > 0) {
+        seconremaining = timerCount;
+        // Clears interval and stops timer
+        StopGame();
+        clearInterval(timer);
+      }
+    } // Tests if time has run out
+    else if (timerCount <= 0) {
+      // Clears interval
+      StopGame();
+      clearInterval(timer);
+    }
+  }, 1000);
+  return;
+}
+
+//This method will execute when timer ends or all the question have been answered
+function StopGame() {
+  questionDiv.classList.add("hide");
+  timer_div.classList.add("hide");
+  record_name.classList.remove("hide");
+  highscore.textContent = `You got ${totalCorrect} correct out of ${totalQuestion} questions in ${seconremaining} seconds remaining`;
+
+  return;
+}
+
+//Save data to local storage
+function SaveData() {
+  if (isEmpty(playername.value)) {
+    alert("Name is required!");
+    return;
+  }
+  var score = localStorage.getItem("HighScore");
+
+  if (isEmpty(score)) {
+    score = [
+      {
+        Name: playername.value,
+        Score: totalCorrect,
+        Total: totalQuestion,
+        Remaining_second: seconremaining,
+        percentage: totalCorrect / totalQuestion,
+      },
+    ];
+    localStorage.setItem("HighScore", JSON.stringify(score));
+  } else {
+    var pscore = JSON.parse(score);
+
+    pscore.push({
+      Name: playername.value,
+      Score: totalCorrect,
+      Total: totalQuestion,
+      Remaining_second: seconremaining,
+      percentage: totalCorrect / totalQuestion,
+    });
+    localStorage.setItem("HighScore", JSON.stringify(pscore));
+  }
+  record_name.classList.add("hide");
+  high_score_div.classList.remove("hide");
+  LoadHighScore();
+  return;
+}
+
+//load question method which helps to load the specific question
+function LoadQuestion(index) {
+  if (totalQuestion > 9 || index > 9) {
+    isWin = true;
+    return;
+  }
+
+  currentquestionid = index;
+  questionDiv.textContent = "";
+  totalQuestion = totalQuestion + 1;
+  var questions = questionList[index];
+
+  var ul = document.createElement("ul");
+  ul.textContent = questions.question;
+
+  for (var i = 0; i < questions.options.length; i++) {
+    var child = document.createElement("li");
+    var button = document.createElement("button");
+    button.textContent = questions.options[i].choice;
+    button.classList.add("choicestyle");
+    button.setAttribute("IsAnswer", questions.options[i].isAnswer);
+    button.setAttribute("QIndex", index);
+    button.addEventListener("click", choice_select);
+    child.appendChild(button);
+    ul.appendChild(child);
+  }
+  var span = document.createElement("span");
+  span.setAttribute("id", "spanResult");
+  questionDiv.appendChild(ul);
+  questionDiv.appendChild(span);
+  return;
+}
+
+//will execute when the select the answer
+function choice_select(event) {
+  var isasner = event.target.getAttribute("IsAnswer");
+  var Qid = event.target.getAttribute("QIndex");
+  if (isasner == "true") {
+    totalCorrect = totalCorrect + 1;
+    LoadQuestion(++currentquestionid);
+    document.querySelector("#spanResult").textContent = "Correct answer!";
+  } else {
+    timerCount = timerCount - wrongAnserdeduction;
+    if (timerCount < 0) {
+      timerCount = 0;
+    }
+    LoadQuestion(++currentquestionid);
+    document.querySelector("#spanResult").textContent = "Wrong answer!";
+  }
+  return;
+}
+
+//load high scores into the table
+function LoadHighScore() {
+  var score = localStorage.getItem("HighScore");
+  btnClearHistory.classList.remove("hide");
+  high_score_div.textContent = "";
+  var h2 = document.createElement("h2");
+  h2.textContent = "High Scores";
+  high_score_div.appendChild(h2);
+  if (isEmpty(score)) {
+    high_score_div.innerHTML = "No records available!";
+    startButton.classList.remove("hide");
+    startButton.setAttribute("data-return", true);
+  } else {
+    startButton.textContent = "Play Again";
+    startButton.setAttribute("data-return", true);
+    startButton.classList.remove("hide");
+    var table = CreateTable();
+
+    var pscore = JSON.parse(score);
+    pscore = pscore.sort(function (a, b) {
+      return a.currentquestionid - b.currentquestionid;
+    });
+    for (var i = 0; i < pscore.length; i++) {
+      var tr = document.createElement("tr");
+      var column1 = document.createElement("td");
+      var column2 = document.createElement("td");
+      var column3 = document.createElement("td");
+      var column4 = document.createElement("td");
+      column1.textContent = pscore[i].Name;
+      column2.textContent = pscore[i].Score;
+      column3.textContent = pscore[i].Total;
+      column4.textContent = pscore[i].Remaining_second;
+      tr.appendChild(column1);
+      tr.appendChild(column2);
+      tr.appendChild(column3);
+      tr.appendChild(column4);
+      table.appendChild(tr);
+    }
+    high_score_div.appendChild(table);
+  }
+  return;
+}
+
+//clear history
+function ClearHistory() {
+  localStorage.setItem("HighScore", "");
+  LoadHighScore();
+  return;
+}
+
+startButton.addEventListener("click", startgame);
+btnSave.addEventListener("click", SaveData);
+btnClearHistory.addEventListener("click", ClearHistory);
+init();
